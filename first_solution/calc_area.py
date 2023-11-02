a18901c441ea802ab9a98b150a2de8af57b09682
PaulRojasUN <paul.rojas@correounivalle.edu.co>
2023-11-02 10:58:13 -0500

    Added first solution to repository

---
+++ first_solution/calc_area.py
@@ -0,0 +1,33 @@
+import math
+import time
+
+def f(x):
+    return (math.sin(x)**3) + (math.cos(x)**3)
+
+def calc_area(a, b, rectangles, f):
+    
+    start = time.time()
+
+    delta_x = (b - a)/rectangles
+
+    acc_area = 0
+
+    k = delta_x
+
+    while (k <= b):
+        acc_area += delta_x*f(k)
+        k += delta_x
+        
+    
+    print("The area is " + str(acc_area) + " units")
+
+    end = time.time()
+
+    print("Elapsed time: " + str(end-start))
+
+
+# CALL
+
+calc_area(0,2, 9000000000, f)
+
+
